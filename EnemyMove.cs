c7ce895dd9446f89d0c5b49551365bff77447dc4
Denise Gip <denisegip0818@gmail.com>
2020-11-26 17:46:42 -0800

    added enemy movement

---
+++ EnemyMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public bool MoveRight;
+
+    // Use this for initialization
+    void Update()
+    {
+        // Use this for initialization
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2((float)1.25, (float)1.25);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2((float)-1.25, (float)1.25);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.CompareTag("turn"))
+        {
+
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
+    }
+}
